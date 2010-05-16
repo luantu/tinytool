fcc727e4d5a2d876d7cb230b11fbec3d25892e4c
programus <programus@2fd5ec71-e839-3aad-1786-e3cec211a5e1>
2010-05-16 05:30:12 +0000

    Add about dialog.

--- windowsTools/vc/ScreenRuler/ScreenRuler/resource.h
+++ windowsTools/vc/ScreenRuler/ScreenRuler/resource.h
@@ -2,18 +2,18 @@
 // Microsoft Visual C++ generated include file.
 // Used by ScreenRuler.rc
 //
-#define IDD_SCREENRULER_DIALOG          102
 #define IDS_APP_TITLE                   103
 #define IDS_COPY                        104
+#define IDS_ABOUT                       105
+#define IDS_ABOUT_TITLE                 106
 #define IDC_SCREENRULER                 109
 #define IDR_MAINFRAME                   128
 #define IDB_POINT                       129
 #define IDB_LABEL                       130
 #define IDI_SCREENRULER                 132
-#define IDM_FILE_EXIT                   32771
-#define IDM_EXIT                        32772
-#define IDM_COPY                        32773
-#define IDC_STATIC                      -1
+#define IDM_EXIT                        32771
+#define IDM_COPY                        32772
+#define IDM_ABOUT                       32773
 
 // Next default values for new objects
 // 
@@ -22,7 +22,7 @@
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        135
 #define _APS_NEXT_COMMAND_VALUE         32774
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
